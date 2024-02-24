ff20da6ce8efddb559aa960737d3e65a775663aa
SamTseng <samtseng@ntnu.edu.tw>
2024-02-24 11:12:19 +0800

    change fields for new Scopus format

--- src/scopus2wos.pl
+++ src/scopus2wos.pl
@@ -8,30 +8,33 @@ use Text::CSV;
 $file = $ARGV[0]; # 'tmp/scopus_eng_20211102.csv'
 print(STDERR "file to read: $file\n");
 
-@Scopus_Fields = split(",", "Authors,Author(s) ID,Title,Year,Source title,Volume,Issue,Art. No.,Page start,Page end,Page count,Cited by,DOI,Link,Affiliations,Authors with affiliations,Abstract,Author Keywords,Index Keywords,Molecular Sequence Numbers,Chemicals/CAS,Tradenames,Manufacturers,Funding Details,Funding Text 1,Funding Text 2,Funding Text 3,Funding Text 4,Funding Text 5,Funding Text 6,References,Correspondence Address,Editors,Sponsors,Publisher,Conference name,Conference date,Conference location,Conference code,ISSN,ISBN,CODEN,PubMed ID,Language of Original Document,Abbreviated Source Title,Document Type,Publication Stage,Open Access,Source,EID");
+# @Scopus_Fields = split(",", "Authors,Author(s) ID,Title,Year,Source title,Volume,Issue,Art. No.,Page start,Page end,Page count,Cited by,DOI,Link,Affiliations,Authors with affiliations,Abstract,Author Keywords,Index Keywords,Molecular Sequence Numbers,Chemicals/CAS,Tradenames,Manufacturers,Funding Details,Funding Text 1,Funding Text 2,Funding Text 3,Funding Text 4,Funding Text 5,Funding Text 6,References,Correspondence Address,Editors,Sponsors,Publisher,Conference name,Conference date,Conference location,Conference code,ISSN,ISBN,CODEN,PubMed ID,Language of Original Document,Abbreviated Source Title,Document Type,Publication Stage,Open Access,Source,EID");
+# On 2024/02/24, replace the above line with the next line:
+@Scopus_Fields = split(",", "Authors,Author full names,Author(s) ID,Title,Year,Source title,Volume,Issue,Art. No.,Page start,Page end,Page count,Cited by,DOI,Link,Affiliations,Authors with affiliations,Abstract,Author Keywords,Index Keywords,Molecular Sequence Numbers,Chemicals/CAS,Tradenames,Manufacturers,Funding Details,Funding Texts,References,Correspondence Address,Editors,Publisher,Sponsors,Conference name,Conference date,Conference location,Conference code,ISSN,ISBN,CODEN,PubMed ID,Language of Original Document,Abbreviated Source Title,Document Type,Publication Stage,Open Access,Source,EID");
 
 $num_lines = -1;
 # Read/parse CSV
-$csv = Text::CSV->new ({ binary => 1, auto_diag => 1 });
+$csv = Text::CSV->new ({ binary => 1, auto_diag => 1, sep_char  => ',', 
+                        quote_char => q{"}, allow_loose_quotes => 1 });
 open $fh, "<:encoding(utf8)", $file or die "$file: $!";
 while ($row = $csv->getline ($fh)) {
     if ($num_lines == -1) { $num_lines += 1; next; }
     $AF = join("\n   ", split(", ", $row->[0]));
-    $C1 = &convertC1($row->[15]);
-    $CR = join("\n   ", split("; ", $row->[30]));
+    $C1 = &convertC1($row->[16]);
+    $CR = join("\n   ", split("; ", $row->[26]));
     $num_lines += 1;
     print("
 AF $AF
-TI $row->[2]
-PY $row->[3]
-SO $row->[4]
+TI $row->[3]
+PY $row->[4]
+SO $row->[5]
 C1 $C1
-LA $row->[43]
-J9 $row->[44]
-DT $row->[45]
-AB $row->[16]
-DE $row->[17]
-ID $row->[18]
+LA $row->[39]
+J9 $row->[40]
+DT $row->[41]
+AB $row->[17]
+DE $row->[18]
+ID $row->[19]
 CR $CR
 UT ISI:$num_lines
 ER
